10f470142c173751c3c50c3c6611a1699bc90130
ben <47470625+gwoob@users.noreply.github.com>
2022-09-22 08:16:12 -0700

    Update zodiac-calculator.c

--- zodiac-calculator.c
+++ zodiac-calculator.c
@@ -26,76 +26,76 @@ int main() {
         printf("hmmm.. okay let me see here.\n");
 
         if (birthMonth == 1 && birthDay <= 20) {
-            printf("capricorn!");
+            printf("capricorn!\n");
 
         } else if (birthMonth == 1 && birthDay >= 21) {
-            printf("aquarius!");
+            printf("aquarius!\n");
 
         } else if (birthMonth == 2 && birthDay <= 19) {
-            printf("aquarius!");
+            printf("aquarius!\n");
 
         } else if (birthMonth == 2 && birthDay >= 20) {
-            printf("pisces!");
+            printf("pisces!\n");
 
         } else if (birthMonth == 3 && birthDay <= 20) {
-            printf("pisces!");
+            printf("pisces!\n");
 
         } else if (birthMonth == 3 && birthDay >= 21) {
-            printf("aries!");
+            printf("aries!\n");
 
         } else if (birthMonth == 4 && birthDay <= 20) {
-            printf("aries!");
+            printf("aries!\n");
 
         } else if (birthMonth == 4 && birthDay >= 21) {
-            printf("taurus!");
+            printf("taurus!\n");
 
         } else if (birthMonth == 5 && birthDay <= 20) {
-            printf("taurus!");
+            printf("taurus!\n");
 
         } else if (birthMonth == 5 && birthDay >= 21) {
-            printf("gemini!");
+            printf("gemini!\n");
 
         } else if (birthMonth == 6 && birthDay <= 21) {
-            printf("gemini!");
+            printf("gemini!\n");
 
         } else if (birthMonth == 6 && birthDay >= 22) {
-            printf("cancer!");
+            printf("cancer!\n");
 
         } else if (birthMonth == 7 && birthDay <= 22) {
-            printf("cancer!");
+            printf("cancer!\n");
 
         } else if (birthMonth == 7 && birthDay >= 23) {
-            printf("leo!");
+            printf("leo!\n");
 
         } else if (birthMonth == 8 && birthDay <= 23) {
-            printf("leo!");
+            printf("leo!\n");
 
         } else if (birthMonth == 8 && birthDay >= 24) {
-            printf("virgo!");
+            printf("virgo!\n");
 
         } else if (birthMonth == 9 && birthDay <= 22) {
-            printf("virgo!");
+            printf("virgo!\n");
 
         } else if (birthMonth == 9 && birthDay >= 23) {
-            printf("libra!");
+            printf("libra!\n");
 
         } else if (birthMonth == 10 && birthDay <= 22) {
-            printf("libra!");
+            printf("libra!\n");
 
         } else if (birthMonth == 10 && birthDay >= 23) {
-            printf("scorpio!");
+            printf("scorpio!\n");
 
         } else if (birthMonth == 11 && birthDay <= 22) {
-            printf("scorpio!");
+            printf("scorpio!\n");
 
         } else if (birthMonth == 11 && birthDay >= 23) {
-            printf("sagittarius!");
+            printf("sagittarius!\n");
 
         } else if (birthMonth == 12 && birthDay <= 21) {
-            printf("sagittarius!");
+            printf("sagittarius!\n");
 
         } else if (birthMonth == 12 && birthDay >= 22) {
-            printf("capricorn!");
+            printf("capricorn!\n");
         }
     }
 
