686497aefd6aa4a31b2f286fb445919871fed6db
ben <47470625+gwoob@users.noreply.github.com>
2022-10-04 08:45:16 -0700

    Update randomizeThreeTeamsOfFour.c

--- randomizeThreeTeamsOfFour.c
+++ randomizeThreeTeamsOfFour.c
@@ -9,6 +9,9 @@ int main()
     int i;
     int j;
 
+    //array of team letters
+    const char teamsArray[] = {'A', 'B', 'C'};
+
     //array of strings with 21 character max per array element
     char namesArray[][20] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
 
@@ -22,52 +25,36 @@ int main()
 
     //seed rand function with system time
     srand(time(NULL));
+
     //set rand max
     j = rand() % 11;
-
 
     //shuffle array elements without any overlap
     for(i = 0; i < 11; i++)
     {
         //copy namesArray[i] (i = 0 first pass) element to a str for safekeeping
         strcpy(str, namesArray[i]);
+
         //copy namesArray[j] (j = rand) element to namesArray[i]
         strcpy(namesArray[i], namesArray[j]);
+
         //copy str to namesArray[j]
         strcpy(namesArray[j], str);
     }
 
-    printf("%s, you are team leader of group A!\n", namesArray[0]);
-
-    //print the other three members of group A in sequence
-    for(i = 1; i <= 3; i++)
+    //print each team's leader and members
+    for(i = 0; i <= 2; i++)
     {
-        printf("%s, ", namesArray[i]);
+        printf("%s, you are team leader of group %c!\n", namesArray[0], teamsArray[i]);
+
+        //print other three members of group
+        for (j = 1; j <= 3; j++)
+        {
+            printf("%s, ", namesArray[j]);
+        }
+
+        printf("you also belong to group %c.\n\n", teamsArray[i]);
     }
-
-    printf("you also belong to group A.\n\n");
-
-
-    printf("%s, you are team leader of group B!\n", namesArray[4]);
-
-    //print the other three members of group B in sequence
-    for(i = 5; i <= 7; i++)
-    {
-        printf("%s, ", namesArray[i]);
-    }
-
-    printf("you also belong to group B.\n\n");
-
-
-    printf("%s, you are team leader of group C!\n", namesArray[8]);
-
-    //print the other three members of group A in sequence
-    for(i = 9; i <= 11; i++)
-    {
-        printf("%s, ", namesArray[i]);
-    }
-
-    printf("you also belong to group C.\n\n");
 
     return 0;
 }
